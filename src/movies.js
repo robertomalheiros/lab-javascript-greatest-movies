b6c09d76c43a414cb94182f7389fbe0b2d1e389d
robertomalheiros <robertomalheirosb@gmail.com>
2022-10-23 15:21:06 -0300

    LAB Completo

--- src/movies.js
+++ src/movies.js
@@ -19,13 +19,19 @@ function howManyMovies(moviesArray) {
 
 // Iteration 3: All scores average - Get the average of all scores with 2 decimals
 function scoresAverage(moviesArray) {
+    let notas = []
     if(!moviesArray.length == 0){
-        let zero = []
-        const mediaUndef = moviesArray.map(nota => nota.score !== undefined?
-            zero.push(nota.score): zero.push(0))    
-        const somaZero = zero.reduce((nAnt, nAtua)=> nAnt+nAtua)
-        const mediaZero = (somaZero / moviesArray.length)
-        return parseFloat(mediaZero.toFixed(2))
+
+        const mediaUndef = moviesArray.filter(filme => filme.score !== undefined)
+
+        const scoreG = mediaUndef.filter(filme => notas.push(filme.score))
+        console.log(notas)
+        if(notas.length == 0 || notas.length != 0){
+
+        const somaNotas = notas.reduce((nAnt, nAtua)=> nAnt+nAtua)
+        const mediaNotas = (somaNotas / mediaUndef.length)
+        return parseFloat(mediaNotas.toFixed(2))
+        }
 
     }
         return 0
@@ -36,14 +42,17 @@ function scoresAverage(moviesArray) {
 function dramaMoviesScore(moviesArray) {
     let listaNota = []
     if(moviesArray.length !== 0){
+        
         const filmeDrama = moviesArray.filter( filmes => filmes.genre.includes("Drama"))
         const scoreG = filmeDrama.filter(filmes => listaNota.push(filmes.score))
+        if(listaNota.length === 0){
+            return 0
+        }
+        
         const somaG = listaNota.reduce((nAnt, nAtua)=> nAnt+nAtua)
         const mediaG = somaG / listaNota.length
-        if(listaNota.length !== 0){
-            return parseFloat(mediaG.toFixed(2))
-        }else if (listaNota == 0){
-            return 0}
+        return parseFloat(mediaG.toFixed(2))
+
         
 
     }return 0
@@ -51,10 +60,35 @@ function dramaMoviesScore(moviesArray) {
 }
 
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
-function orderByYear(moviesArray) {}
+function orderByYear(moviesArray) {
+        const novoArray = moviesArray.map(a => a)
+        novoArray.sort((a, b) => a.year - b.year).sort((a,b) => a.year == b.year? a.title.localeCompare(b.title): a.year - b.year)
+        return novoArray
+    
+}
 
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
-function orderAlphabetically(moviesArray) {}
+function orderAlphabetically(moviesArray) {
+
+        let listaA = []
+        let listaFilmes = []
+        const lista = moviesArray.filter(filme => listaA.includes(filme.title)?
+            filme.title: listaA.push(filme.title)) 
+        listaA.sort((a,b) => a.localeCompare(b))
+       
+        if(moviesArray.length < 19){
+            for(let i = 0; i < moviesArray.length ; i++){
+                listaFilmes.push(listaA[i])}
+                return listaFilmes
+        }
+        else if((moviesArray.length > 19)){
+            for(let i = 0; i < 20 ; i++){
+                listaFilmes.push(listaA[i])}
+                return listaFilmes
+        }
+
+
+}
 
 // BONUS - Iteration 7: Time Format - Turn duration of the movies from hours to minutes
 function turnHoursToMinutes(moviesArray) {}
